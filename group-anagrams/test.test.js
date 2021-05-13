6e554863d7a7d20e802aaa4c2c65d74a18399566
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-05-14 03:52:57 +0700

    added more test cases to groupAnagrams

--- group-anagrams/test.test.js
+++ group-anagrams/test.test.js
@@ -2,5 +2,22 @@ const {groupAnagrams} = require('./index');
 
 test('case 1', () => {
   const strs = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat'];
-  expect(groupAnagrams(strs).toString().split('').sort()).toEqual([['bat'], ['nat', 'tan'], ['ate', 'eat', 'tea']].toString().split('').sort());
+  const resultGroup = groupAnagrams(strs).toString().split('').sort();
+  const expectedResult = [['bat'], ['nat', 'tan'], ['ate', 'eat', 'tea']]
+      .toString().split('').sort();
+  expect(resultGroup).toEqual(expectedResult);
+});
+
+test('case 2', ()=> {
+  const strs = [''];
+  const resultGroup = groupAnagrams(strs).toString().split('').sort();
+  const expectedResult = [['']].toString().split('').sort();
+  expect(resultGroup).toEqual(expectedResult);
+});
+
+test('case 3', ()=> {
+  const strs =['a'];
+  const resultGroup = groupAnagrams(strs).toString().split('').sort();
+  const expectedResult = [['a']].toString().split('').sort();
+  expect(resultGroup).toEqual(expectedResult);
 });
