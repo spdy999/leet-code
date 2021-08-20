31fa9d370d83be83c3d2c920a4260d476b695c30
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-08-20 22:38:55 +0700

    create simple graph data structure

---
+++ data-structure/graph/index.js
@@ -0,0 +1,32 @@
+const airports = 'PHX BKK OKC JFK LAX MEX EZE HEL LOS LAP LIM'.split(' ')
+
+const routes = [
+	['PHX', 'LAX'],
+	['PHX', 'JFK'],
+	['JFK', 'OKC'],
+	['JFK', 'HEL'],
+	['JFK', 'LOS'],
+	['MEX', 'LAX'],
+	['MEX', 'BKK'],
+	['MEX', 'LIM'],
+	['MEX', 'EZE'],
+	['LIM', 'BKK'],
+]
+
+const adjacencyList = new Map()
+
+const addNode = (airport) => {
+	adjacencyList.set(airport, [])
+}
+
+const addEdge = (origin, destination) => {
+	adjacencyList.get(origin).push(destination)
+	adjacencyList.get(destination).push(origin)
+}
+
+airports.forEach(addNode)
+routes.forEach(route => addEdge(...route))
+
+console.log(adjacencyList)
+
+
