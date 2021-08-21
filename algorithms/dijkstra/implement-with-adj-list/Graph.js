7a35ac4a11950cc1cb51ef90c6c8b98e3636cdb5
spdy999 <1a1a1a.262626.0010@gmail.com>
2021-08-21 17:26:12 +0700

    finish graph for dijkstra with adjacency list

--- algorithms/dijkstra/implement-with-adj-list/Graph.js
+++ algorithms/dijkstra/implement-with-adj-list/Graph.js
@@ -1,7 +1,34 @@
 class Graph {
 	constructor() {
 		this.vertices = []
-		this.adjacencyList = new Map()
+		this.adjacencyList = {}
+		// this.adjacencyList = new Map()
+	}
+
+	/**
+	 *
+	 * @param {number} vertex
+	 */
+	addVertex(vertex) {
+		this.vertices.push(vertex)
+		// this.adjacencyList.set(vertex, new Map())
+		this.adjacencyList[vertex] = {}
+	}
+
+	/**
+	 *
+	 * @param {number} source
+	 * @param {number} destination
+	 * @param {number} weight
+	 */
+	addEdge(source, destination, weight) {
+		// this.adjacencyList.get(source).set(destination, weight)
+		this.adjacencyList[source][destination] = weight
+	}
+
+	changeWeight(source, destination, weight) {
+		// this.adjacencyList.get(source).set(destination)
+		this.adjacencyList[source][destination] = weight
 	}
 }
 
